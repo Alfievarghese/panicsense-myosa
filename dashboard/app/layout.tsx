96d989619887f9797f9abb7a176aefb1497f3e1b
Alfie Varghese <alfievarghese22@gmail.com>
2026-08-09 15:36:50 +0530

    feat: add vercel analytics

--- dashboard/app/layout.tsx
+++ dashboard/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -32,6 +33,7 @@ export default function RootLayout({
           <div className="blob-2"></div>
         </div>
         {children}
+        <Analytics />
       </body>
     </html>
   );
